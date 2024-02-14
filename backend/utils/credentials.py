baa98edbf2383e0958e66ff2e85bf0fe9e896894
nibedita6302 <nibedita.6302@gmail.com>
2024-02-14 14:12:23 +0530

    Validate credentials of users -utils

---
+++ backend/utils/credentials.py
@@ -0,0 +1,36 @@
+from bcrypt import hashpw, gensalt, checkpw
+import re
+
+def hash_password(plain_pw):
+    salt = gensalt()
+    return hashpw(plain_pw.encode('utf-8'),salt)
+
+def check_password(plain_pw, hash_pw):
+    return checkpw(plain_pw.encode('utf-8'),hash_pw)
+
+def validate_email(email):
+    if email.count('@')==1:
+        front, back = email.split('@')
+        pattern = re.compile(r'^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*$')
+        # Explaination
+        # Alpha numeric (1 or more) followed by '.' or '_' which is immediately
+        # followed by Alpha numeric characters (1 or more)
+        # No 2 special char occur together.
+        if re.match(pattern, front) and re.match(pattern, back):
+            return True
+    return False
+
+def validate_password(password):
+    pattern = re.compile(r'^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$')
+    # Explanation 
+    # (?=.*[a-z]) - atleast one lowercase letter
+    # (?=.*[A-Z]) - atleast one uppercase letter
+    # (?=.*\d) - atleast one digit
+    # (?=.*[^a-zA-Z0-9]) - atleast one special character (not alphanumeric)
+    # .{8,} - atleast 8 characters long
+    return re.match(pattern, password) is not None
+
+
+# Testing
+# print(validate_email('hello1._2@hello'))
+# print( validate_password('hello12@A') )

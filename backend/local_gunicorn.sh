408db8f2712911e46c6c87f03ba0f79f607043ae
nibedita6302 <nibedita.6302@gmail.com>
2024-02-14 20:24:10 +0530

    Minor changes to linux executale file

--- backend/local_gunicorn.sh
+++ backend/local_gunicorn.sh
@@ -4,7 +4,7 @@ echo "Welcome to to the setup. This will setup the local virtual env."
 echo "And then it will install all the required python libraries."
 echo "You can rerun this without any issues."
 echo "----------------------------------------------------------------------"
-if [ -d "../.env" ];
+if [ -d ".env" ];
 then
     echo "Enabling virtual env"
 else
@@ -13,7 +13,7 @@ else
 fi
 
 # Activate virtual env
-. ../.env/bin/activate
+. .env/bin/activate
 export ENV=stage
 gunicorn main:app --worker-class gevent --bind 0.0.0.0:8000 --workers=2
-deactivate
+# deactivate
--- backend/local_workers.sh
+++ backend/local_workers.sh
@@ -4,7 +4,7 @@ echo "Welcome to to the setup. This will setup the local virtual env."
 echo "And then it will install all the required python libraries."
 echo "You can rerun this without any issues."
 echo "----------------------------------------------------------------------"
-if [ -d "../.env" ];
+if [ -d ".env" ];
 then
     echo "Enabling virtual env"
 else
@@ -13,7 +13,7 @@ else
 fi
 
 # Activate virtual env
-. ../.env/bin/activate
+. .env/bin/activate
 export ENV=development
 celery -A main.celery worker -l info
-deactivate
+# deactivate

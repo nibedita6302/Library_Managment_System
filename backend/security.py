c66c1280e83e74e69ec4abd7c318590f38f2757f
nibedita6302 <nibedita.6302@gmail.com>
2024-02-14 14:05:31 +0530

    Setup security.py

---
+++ backend/security.py
@@ -0,0 +1,8 @@
+
+from flask_security import SQLAlchemySessionUserDatastore
+from .application.models.users import Users, Role
+from .application.database import db
+
+# Setup Flask-Security
+datastore = SQLAlchemySessionUserDatastore(db.session, Users, Role)
+

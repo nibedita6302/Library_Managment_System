366a75463dfba5a7a62adfb560280ea73cb96083
nibedita6302 <nibedita.6302@gmail.com>
2024-02-15 23:55:30 +0530

    Models relationship correction

--- backend/application/models/users.py
+++ backend/application/models/users.py
@@ -4,11 +4,11 @@ from utils.credentials import check_password
 from flask_security import UserMixin, RoleMixin
 from sqlalchemy import Integer, String, Boolean, DateTime, Float, ForeignKey,  Column
 
-class RoleUsers(db.Model):
-    __tablename__='role_user'
-    user_id = Column(Integer(), ForeignKey('users.id'), primary_key=True) # users
-    role_id = Column(Integer(), ForeignKey('role.id'), primary_key=True) # role
-     
+RoleUsers = db.Table('role_user',
+    Column('user_id', Integer(), ForeignKey('users.id'), primary_key=True), # users
+    Column('role_id', Integer(), ForeignKey('role.id'), primary_key=True) # role
+)
+
 class Users(db.Model, UserMixin):
     __tablename__ = 'users'
     id = Column(Integer(), autoincrement=True, primary_key=True)
@@ -18,8 +18,8 @@ class Users(db.Model, UserMixin):
     active = Column(Boolean, nullable=False, default=True) 
     fs_uniquifier = Column(String(255), unique=True) 
     #relationships
-    roles = db.relationship('Role', secondary=RoleUsers, backref='assignedTo', cascade='all, delete-orphan')
-    mybooks = db.relationship('Books', secondary=UserBook, backref='issuedBy', cascade='all, delete-orphan')
+    roles = db.relationship('Role', secondary=RoleUsers, backref='assignedTo', cascade='all, delete')
+    user_book = db.relationship('UserBook', back_populates='issuer', cascade='all, delete')
     
     def match_password(self, password):
         return  check_password(password, self.password)

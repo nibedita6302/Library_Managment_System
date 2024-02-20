f96a5563ce27aac25624f2b31e96f388d7fc5e6f
nibedita6302 <nibedita.6302@gmail.com>
2024-02-20 22:21:27 +0530

    Added user registration route to configure_routes

--- backend/application/api/configure_routes.py
+++ backend/application/api/configure_routes.py
@@ -1,8 +1,7 @@
-from users import Login, Logout
+from .users import UserRegister
 
 def config_all_resource(api):
     user_api(api)
 
 def user_api(api):
-    api.add_resource(Login, "/login")
-    api.add_resource(Logout, "/logout")
+    api.add_resource(UserRegister, "/user-registration")

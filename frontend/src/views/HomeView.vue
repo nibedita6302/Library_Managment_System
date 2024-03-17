f1f64c0e790c5b022adab61301f1a1afb5347c83
nibedita6302 <nibedita.6302@gmail.com>
2024-03-17 22:44:12 +0530

    Home Views

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <PopupMessage v-if="showPopup"
+        message="This is a demo popup"
+        showConfirm="confirm"
+        confirm_text="Confirm Button"
+        @cancel="handleCancel"
+        @confirm="handleConfirm"></PopupMessage>
+  <!-- <SearchBar></SearchBar> -->
+  <!-- <Category></Category> -->
+  <h1>This is Home View Page</h1>
+  <button @click="demoPop">Show pop-up</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import {mapGetters, mapMutations, mapState} from 'vuex';
+import PopupMessage from '@/components/PopupMessage.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{
+    PopupMessage,
+  },
+  data(){
+    return {
+      showPopup: false,
+      confirm: true
+    }
+  },
+
+  methods:{
+    demoPop(){
+      if (this.showPopup==false){
+        this.showPopup=true;
+      }
+    },
+    handleCancel(){
+      this.showPopup = false;
+      console.log('cancel');
+    },
+    handleConfirm(){
+      this.showPopup = false;
+      console.log('confirm');
+    }
   }
+  // computed: {
+  //   ...mapState('searching',['showProducts']),
+  //   ...mapGetters('auth',['GET_USER_ROLE']),
+  // }
 }
 </script>
+
+<style scoped>
+  
+</style>

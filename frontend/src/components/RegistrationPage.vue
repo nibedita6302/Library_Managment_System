3708b78007bedb4b7f69df7f6f79bffb661e0600
nibedita6302 <nibedita.6302@gmail.com>
2024-03-24 14:39:02 +0530

    Register new user API and popup modification - tested

--- frontend/src/components/RegistrationPage.vue
+++ frontend/src/components/RegistrationPage.vue
@@ -1,5 +1,7 @@
 <template>
-    <PopupMessage v-if="showPopup" :message="message" @cancel="handelCancel"></PopupMessage>
+    <PopupMessage v-if="showPopup" :message="message" :cancel_text="cancel_text"
+    :show-confirm="show_confirm" :confirm_text="confirm_text" :alert_type="alert_type"
+    @confirm="handelConfirm" @cancel="handelCancel"></PopupMessage>
     
     <div class="container d-flex justify-content-center">
         <form class="h-100" @submit.prevent="registerUser">
@@ -22,7 +24,7 @@
                     <input type="password" class="form-control" id="password" v-model="password" />
                 </div>
             </div>
-            <button type="submit" class="btn btn-warning" @click="loginUser">Register</button>
+            <button type="submit" class="btn btn-warning" @click="registerUser">Register</button>
         </form>
     </div>
 </template>
@@ -42,7 +44,11 @@ export default {
             email: '',
             password: '',
             message: '',
-            showPopup: false
+            show_confirm: false,
+            cancel_text: null,
+            confirm_text: null,
+            showPopup: false,
+            alert_type: null
         }
     },
     methods: {
@@ -51,18 +57,58 @@ export default {
             if (this.password==''||this.email==''||this.name==''){
                 this.message = 'All field are compulsory!'
                 this.showPopup=true;
-                console.log(this.message, this.showPopup);
+                // console.log(this.message, this.showPopup);
             }
-            else { this.register(); }
+            else { 
+                this.message = 'Registering New User will logout your current session. \
+                Are you sure you want to continue?'
+                this.showPopup=true;
+                this.show_confirm=true;
+                this.cancel_text="Cancel";
+                this.confirm_text='Continue';
+            }
         },
         async register(){
-            console.log('Login API Called');
-            this.message = 'Successfully Registered! Login Now!'
-            this.showPopup=true;
+            try{
+                const res = await fetch('http://localhost:8000/api/user-registration', {
+                    method: 'POST',
+                    mode: 'cors',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type':'application/json',
+                    },
+                    body: JSON.stringify({
+                        'name': this.name,
+                        'email':this.email,
+                        'password': this.password
+                    })
+                })
+                if (!res.ok && res.status!=400) { throw Error("HTTP Error at Search:"+res.status) }
+                const data = await res.json() ;
+                if (res.status==400){
+                    this.showPopup=true;
+                    this.alert_type='danger'
+                    this.message = data.message.error;     // set error message
+                }
+                else {    
+                    this.showPopup=true;
+                    this.alert_type='success';
+                    this.message = data.message.success;   // set success message
+                }
+            }catch(error){console.log(error);} 
         },
         handelCancel(){
             this.showPopup=false;
+            this.alert_type=null;
             console.log('canceled!')
+        },
+        handelConfirm(){
+            this.showPopup=false;
+            this.alert_type=null;
+            this.show_confirm=false;
+            this.cancel_text=null;
+            this.confirm_text=null;
+            this.register(); 
         }
     }
 }

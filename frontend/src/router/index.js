f3df22d8a8dd30de71b469a9e011c2678ee4812e
nibedita6302 <nibedita.6302@gmail.com>
2024-03-26 21:04:48 +0530

    Issue/MyBooks page and router link config

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
 import LoginView from '@/views/LoginView.vue';
 import SearchPage from '@/components/SearchPage.vue';
 import BookDetailsView from '@/views/BookDetailsView.vue';
 import BookList from '@/views/BookList.vue';
+import IssuesView from '@/views/IssuesView.vue';
 
 const routes = [
   { path: '/', name: 'HomeView' ,component: HomeView },
   { path: '/search', name: 'SearchPage', component: SearchPage },
   { path: '/book/:book_id', name: 'BookDetailsView', component: BookDetailsView },
   { path: '/section/:section_id/books', name: 'BookList', component: BookList },
-  // { path: '/mybooks', component: MyBooksView },
+  { path: '/mybooks', component: IssuesView },
   // { path: '/mystats', component: MyStatsView },
   // { path: '/myprofile', component: MyProfileView },
-  // { path: '/issues', component: IssuesView },
+  { path: '/issues', component:IssuesView },
   // { path: '/analytics', component: AnalyticsView },
   { path: '/login', name: 'LoginView' ,component: LoginView },
 

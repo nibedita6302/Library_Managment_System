01415c81955a6f5da236f24816caebc39b5996dc
nibedita6302 <nibedita.6302@gmail.com>
2024-03-28 23:07:25 +0530

    Celery periodic task to delete non-pending issues

--- backend/application/jobs/workers.py
+++ backend/application/jobs/workers.py
@@ -2,7 +2,8 @@ from celery import Celery
 from flask import current_app as app
 
 celery = Celery('Digital Library App', include=['application.jobs.Tasks.asyncDownload',
-                                                'application.jobs.Tasks.timelyEmails'])
+                                                'application.jobs.Tasks.periodicTasks',
+                                                'application.jobs.Tasks.bookIssueStatusEmail'])
 
 # Create a subclass of the task 
 # that wraps the task execution in an application context.

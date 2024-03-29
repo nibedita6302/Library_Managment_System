480f00478a8832b87c135a16af22f6af5c0c39ca
nibedita6302 <nibedita.6302@gmail.com>
2024-03-29 22:25:37 +0530

    Added update delete button to section, while avoiding streched link

--- frontend/src/components/SectionDisplay.vue
+++ frontend/src/components/SectionDisplay.vue
@@ -2,11 +2,17 @@
     <div class="container p-4">
         <div class="row row-col-3 g-0">
             <div class="col-sm-4 card text-white me-3" v-for="s in sections" :key="s.s_id">
-                <img :src="require('@/assets/upload/'+s.s_image)" class="card-img" :alt="s.s_name">
-                <div class="card-img-overlay">
-                    <h3 class="card-title">{{s.s_name}}</h3>
-                    <b class="card-text">Book Count: {{ s.book_count }}</b>
-                    <router-link :to="'/section/'+s.s_id+'/books'" class="stretched-link"></router-link>
+                <div style="transform: rotate(0);">
+                    <img :src="require('@/assets/upload/'+s.s_image)" class="card-img" :alt="s.s_name">
+                    <div class="card-img-overlay">
+                        <h3 class="card-title">{{s.s_name}}</h3>
+                        <b class="card-text">Book Count: {{ s.book_count }}</b>
+                        <router-link :to="'/section/'+s.s_id+'/books'" class="stretched-link"></router-link>
+                    </div>
+                </div>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-warning" @click.prevent="updateSection($event)">Update</button>
+                    <button type="button" class="btn btn-danger" @click.prevent="deleteSection">Delete</button>
                 </div>
             </div>
         </div>
@@ -19,7 +25,8 @@ export default{
     name: 'SectionDisplay',
     data(){
         return{
-            sections: []
+            sections: [],
+            user: JSON.parse(localStorage.getItem('user'))
         }
     },
     methods:{
@@ -34,6 +41,11 @@ export default{
                 const data = await res.json() ;
                 this.sections = data;  // set output
             }catch(error){console.log(error);} 
+        }, 
+        async updateSection(event){
+            
+        },
+        async deleteSection(){
         }
     },
     created(){
@@ -43,4 +55,7 @@ export default{
 </script>
 
 <style scoped>
+img{
+    min-height: 150px;
+}
 </style>

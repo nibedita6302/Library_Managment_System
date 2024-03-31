ea9f334ba01d539bc90695ab94130ba3f8f44882
nibedita6302 <nibedita.6302@gmail.com>
2024-03-31 17:27:53 +0530

    Implemented CRUD on User Profile

---
+++ frontend/src/views/MyProfile.vue
@@ -0,0 +1,24 @@
+<template>
+    <UserProfileCRUD></UserProfileCRUD>
+</template>
+
+<script>
+import UserProfileCRUD from '@/components/UserProfileCRUD.vue'
+
+export default{
+    name: 'MyProfile',
+    data(){
+        return{
+            user: JSON.parse(localStorage.getItem('user')),
+            token: localStorage.getItem('auth_token')
+        }
+    },
+    components:{
+        UserProfileCRUD,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
